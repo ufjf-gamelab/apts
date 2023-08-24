aa75de01bdf058220116edcb21f0e6f8053e5936
Igor Knop <igorknop@gmail.com>
2023-08-24 15:00:44 -0300

    change ts config to selfplay and jest

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type {Config} from 'jest';
 export default async (): Promise<Config> => {
   return {
     verbose: true,
-    preset: 'ts-jest',
+    preset: 'ts-jest/presets/js-with-ts',
     testEnvironment: 'node',
     transform: {
       '^.+\\.tsx?$': 'ts-jest',
