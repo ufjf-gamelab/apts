03ccd7ed7cb20ee8f65fd4dc8b6281bdf0b7167a
Gabriel Malosto <gabriel.malosto@estudante.ufjf.br>
2024-10-12 18:02:32 -0300

    Initialize branch

---
+++ eslint.config.js
@@ -0,0 +1,47 @@
+/** @type {import("eslint").Linter.Config} */
+import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    files: ["src/**/*.{js,mjs,cjs,ts}"],
+  },
+  {
+    ignores: ["prettier.config.js"],
+  },
+
+  eslint.configs.all,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+
+  {
+    files: ["eslint.config.{js,cjs}"],
+    parserOptions: {
+      sourceType: "script",
+    },
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.nodeBuiltin,
+      },
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+        sourceType: "module",
+        ecmaVersion: "latest",
+      },
+    },
+  },
+  {
+    rules: {
+      "no-ternary": "off",
+      "no-warning-comments": "warn",
+      "one-var": "off",
+      "sort-imports": "off",
+    },
+  },
+  eslintConfigPrettier,
+];
