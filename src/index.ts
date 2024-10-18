c7874ca6835a1425524baed4ab6cf283b699975e
Gabriel Malosto <professional@gabdumal.com>
2024-10-18 11:51:38 -0300

    Create schema for describing arguments for TicTacToe game.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,88 @@
-import { Argument, Command, Option, program } from "commander";
+import {
+  Argument,
+  Command,
+  InvalidArgumentError,
+  Option,
+  program,
+} from "commander";
+import dotenv from "dotenv";
+import fs, { ReadStream, WriteStream } from "fs";
+import path from "path";
 import play from "./interface/play";
 
-enum GameNames {
+enum GameName {
   Connect4 = "connect4",
   TicTacToe = "tictactoe",
 }
+
+enum FileOperation {
+  Read = "r",
+  Write = "wx",
+  Overwrite = "w+",
+}
+
+const createReadStream = (filePath: string): ReadStream => {
+  try {
+    return fs.createReadStream(filePath);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      throw new InvalidArgumentError(
+        `\n  Failed to create read stream: ${error.message}`,
+      );
+    }
+    throw new InvalidArgumentError(
+      "\n  An unknown error occurred while creating read stream.",
+    );
+  }
+};
+
+const createWriteStream = (filePath: string): WriteStream => {
+  try {
+    return fs.createWriteStream(filePath);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      throw new InvalidArgumentError(
+        `\n  Failed to create write stream: ${error.message}`,
+      );
+    }
+    throw new InvalidArgumentError(
+      "\n  An unknown error occurred while creating write stream.",
+    );
+  }
+};
+
+const validateFilePath = (
+  filePath: string,
+  fileOperation: FileOperation,
+): ReadStream | WriteStream => {
+  const resolvedPath = path.resolve(filePath);
+
+  if (fileOperation === FileOperation.Read) {
+    if (!fs.existsSync(resolvedPath)) {
+      throw new InvalidArgumentError("\n  The file path does not exist.");
+    }
+    return createReadStream(resolvedPath);
+  }
+
+  // Attempt to create a file to check if it's writable
+  try {
+    fs.writeFileSync(resolvedPath, "", { flag: FileOperation.Overwrite });
+    return createWriteStream(resolvedPath);
+  } catch (error: unknown) {
+    if (error instanceof Error && "code" in (error as NodeJS.ErrnoException)) {
+      if ((error as NodeJS.ErrnoException).code === "EEXIST") {
+        throw new InvalidArgumentError(
+          `\n  The file already exists and cannot be overwritten.`,
+        );
+      }
+
+      throw new InvalidArgumentError(
+        `\n  Failed to create write stream:\n  ${error.message}`,
+      );
+    }
+    throw new InvalidArgumentError("\n  An unknown error occurred.");
+  }
+};
 
 program
   .name("apts-engine")
@@ -22,19 +100,40 @@ interface CommandDefinition {
 const commandsDefinitions: CommandDefinition[] = [
   {
     arguments: [
+      new Argument("<game>", "The game to be implemented.").choices([
+        GameName.Connect4,
+        GameName.TicTacToe,
+      ]),
+      new Argument("<output>", "The output file path.").argParser(
+        (filePath: string) => validateFilePath(filePath, FileOperation.Write),
+      ),
+    ],
+    command: new Command("desc-file")
+      .description("Get the description file for implementing a new game.")
+      .action((game: GameName, output: WriteStream) => {
+        if (game === GameName.TicTacToe) {
+          const description = {
+            $schema: `${process.env.SCHEMAS_DIRECTORY}/TicTacToe/schema.json`,
+          };
+          output.write(JSON.stringify(description));
+        }
+      }),
+  },
+  {
+    arguments: [
       new Argument("<game>", "The game to be played.").choices([
-        GameNames.Connect4,
-        GameNames.TicTacToe,
+        GameName.Connect4,
+        GameName.TicTacToe,
       ]),
     ],
     command: new Command("play")
       .description("Play a game.")
       .action((game: string) => {
-        switch (game as GameNames) {
-          case GameNames.Connect4:
+        switch (game as GameName) {
+          case GameName.Connect4:
             console.log("Playing Connect 4...");
             break;
-          case GameNames.TicTacToe:
+          case GameName.TicTacToe:
             play();
             break;
           default:
@@ -67,5 +166,7 @@ commandsDefinitions.forEach(commandDefinition => {
   }
   program.addCommand(commandDefinition.command);
 });
+
+dotenv.config();
 
 program.parse();
